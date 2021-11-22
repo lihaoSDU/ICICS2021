7f8004f982dccae3dd60c54b18b08edefb15c288
lihaoSDU <86996308+lihaoSDU@users.noreply.github.com>
2021-11-22 22:31:49 +0800

    Create config.py

---
+++ fuzz/utils/config.py
@@ -0,0 +1,27 @@
+
+batch_size = 32
+epochs = 200
+lr = 0.01
+momentum = 0.9
+no_cuda = False
+cuda_id = '0'
+seed = 1
+log_interval = 10
+l2_decay = 5e-4
+class_num = 31
+param = 0.3
+bottle_neck = True
+root_path = "/home/leohoo/work/code/tlfuzz/datasets/Office31/"
+source_name = "webcam"
+target_name = "dslr"
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
