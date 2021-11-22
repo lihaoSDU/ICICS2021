b9e5cdbea0ca8283e618e309dfcbc07dd502a006
lihaoSDU <86996308+lihaoSDU@users.noreply.github.com>
2021-11-22 22:18:07 +0800

    Create config.py

---
+++ DSAN/utils/config.py
@@ -0,0 +1,16 @@
+
+batch_size = 32
+epochs = 100
+lr = 0.01
+momentum = 0.9
+no_cuda = False
+cuda_id = '0'
+seed = 1
+log_interval = 10
+l2_decay = 5e-4
+class_num = 65
+param = 0.3
+bottle_neck = True
+root_path = "/home/leohoo/work/code/tlfuzz/datasets/office_home/"
+# source_name = "Product"
+# target_name = "RealWorld"
