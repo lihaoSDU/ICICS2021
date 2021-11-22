be0752c70cf1ef0d58ba96f2c942450363af24e9
lihaoSDU <86996308+lihaoSDU@users.noreply.github.com>
2021-11-22 22:11:09 +0800

    Create run.sh

---
+++ DSAN/run.sh
@@ -0,0 +1,2 @@
+python3 train_target_model.py --model_name alexnet --epochs 200 --data_path /datasets/office_home/Product/ --cate_name alexnet_product
+python3 train_target_model.py --model_name vgg19 --epochs 200 --data_path /datasets/office_home/Product/ --cate_name vgg_product
